8f2b80a38cc45c46d8c63aca978f7a8a3ee9b4a1
dimagarn <56264897+dimagarn@users.noreply.github.com>
2023-12-22 03:17:04 +0500

    Streamlit realization

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from transformers import pipeline
+import streamlit as st
 
-classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+def classify(sentences):
+    classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
 
-sentences = ["Hello everyone, and today i want to introduce you my new amazing product!"]
+    model_outputs = classifier(sentences)
+    return model_outputs
 
-model_outputs = classifier(sentences)
-print(model_outputs[0])
+def print_predictions(predictions):
+    for elem in predictions:
+        for item in elem:
+            st.write(f"{item.get('label').title()}: {item.get('score')}")
+
+st.title("Классификатор настроения текста")
+sentences = st.text_input('Предложение')
+classified = classify(sentences)
+if (st.button('Отправить') and sentences) or sentences:
+    st.write("Настроение предложения: ")
+    print_predictions(classified)
