ef68a70c0c1d83f98407470d62632de33537c9d5
Bulrush3 <mike10.09.2000@yandex.ru>
2023-12-24 16:07:20 +0500

    fixing_errors

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
+from fastapi import FastAPI
 from transformers import pipeline
-import streamlit as st
+from pydantic import BaseModel
 
 
+class Item(BaseModel):
+    text: str
 
-def classify(sentences):
-    classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
 
-    model_outputs = classifier(sentences)
-    return model_outputs
+app = FastAPI()
+classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
 
-def print_predictions(predictions):
-    for elem in predictions:
-        for item in elem:
-            st.write(f"{item.get('label').title()}: {item.get('score')}")
 
-st.title("Классификатор настроения текста")
-sentences = st.text_input('Предложение')
-classified = classify(sentences)
-if (st.button('Отправить') and sentences) or sentences:
-    st.write("Настроение предложения: ")
-    print_predictions(classified)
+@app.get("/")
+async def root():
+    return {'model': 'Text Classifier'}
+
+
+#@app.get("/predict/")
+#def predict():
+#    return classifier('I like dogs!')[0]
+
+
+@app.post("/predict/")
+def predict(item: Item):
+    """Text Classifier"""
+    return classifier(item.text)[0]
