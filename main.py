43272a371de5f45a0b09c90481f3172e48b3b45c
Bulrush3 <mike10.09.2000@yandex.ru>
2023-12-24 14:23:45 +0500

    tests_added

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 from transformers import pipeline
-import streamlit as st
+# import streamlit as st
+from fastapi import FastAPI
+from pydantic import BaseModel
 
-def classify(sentences):
-    classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+class Item(BaseModel):
+    text: str
 
-    model_outputs = classifier(sentences)
-    return model_outputs
 
-def print_predictions(predictions):
-    for elem in predictions:
-        for item in elem:
-            st.write(f"{item.get('label').title()}: {item.get('score')}")
+app = FastAPI()
+classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
 
-st.title("Классификатор настроения текста")
-sentences = st.text_input('Предложение')
-classified = classify(sentences)
-if (st.button('Отправить') and sentences) or sentences:
-    st.write("Настроение предложения: ")
-    print_predictions(classified)
+
+@app.get("/")
+async def root():
+    return {'model': 'Text Classifier'}
+
+# def classify(sentences):
+#     classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+
+#     model_outputs = classifier(sentences)
+#     return model_outputs
+
+# def print_predictions(predictions):
+#     for elem in predictions:
+#         for item in elem:
+#             st.write(f"{item.get('label').title()}: {item.get('score')}")
+
+# st.title("Классификатор настроения текста")
+# sentences = st.text_input('Предложение')
+# classified = classify(sentences)
+# if (st.button('Отправить') and sentences) or sentences:
+#     st.write("Настроение предложения: ")
+#     print_predictions(classified)
+
+@app.get("/predict/")
+def predict():
+   return classifier('I like dogs!')[0]
+
+
+@app.post("/predict/")
+def predict(item: Item):
+    """Text Classifier"""
+    return classifier(item.text)[0]
